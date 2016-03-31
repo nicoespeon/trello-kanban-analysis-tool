b7effb4ddaff2dcef0b943d4265650b2553787a2
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2016-03-31 23:34:52 +0200

    Make it work with hardcoded displayed lists

--- app/application.js
+++ app/application.js
@@ -1,7 +1,8 @@
 import Cycle from '@cycle/core';
-import {makeDOMDriver, button} from '@cycle/dom';
+import {makeDOMDriver, div, button, select, option, label} from '@cycle/dom';
 import moment from 'moment';
-import Rx from 'rx';
+import {Observable} from 'rx';
+import R from 'ramda';
 
 import {makeTrelloDriver} from './drivers/trello';
 import {makeGraphDriver} from './drivers/graph';
@@ -10,25 +11,78 @@ import logDriver from './drivers/log';
 import {parseActions} from './models/trello';
 import {parseTrelloData} from './models/graph';
 
-function buttonView ( state$ ) {
-  return state$.map( () => button( 'Get actions' ) )
+function selectView ( id, labelText, match, lists ) {
+  return div( [
+    label( { htmlFor: id }, labelText ),
+    select(
+      { id: id, name: id },
+      [ R.map( name => R.cond( [
+        [ R.equals( match ), R.always( option( { selected: true }, name ) ) ],
+        [ R.T, R.always( option( name ) ) ]
+      ] )( name ), lists ) ]
+    )
+  ] );
 }
 
 function main ( { DOM, Trello } ) {
-  const buttonClicks$ = DOM.select( 'button' ).events( 'click' ).startWith( false );
+  // TODO - refactor these with components (button, select, trello)
+  const buttonClicks$ = DOM
+    .select( 'button' )
+    .events( 'click' )
+    .startWith( false );
 
-  const displayedLists = [ "Backlog", "Card Preparation", "Production", "Tests QA", "Mise en live", "In Production", "Live (March 2016)" ];
-  const trelloActions$ = Rx.Observable.combineLatest(
-    Trello.lists$.startWith( [] ),
+  const firstDisplayedListChanges$ = DOM
+    .select( '#first-displayed-list' )
+    .events( 'input' )
+    .map( ev => ev.target.value )
+    .startWith( "Backlog" );
+
+  const lastDisplayedListChanges$ = DOM
+    .select( '#last-displayed-list' )
+    .events( 'input' )
+    .map( ev => ev.target.value )
+    // TODO - take last column value by default
+    .startWith( "Live (March 2016)" );
+
+  const trelloLists$ = Trello.lists$.startWith( [] );
+
+  const displayedLists$ = Observable.combineLatest(
+    trelloLists$,
+    firstDisplayedListChanges$,
+    lastDisplayedListChanges$,
+    // TODO - implement this
+    () => [ "Backlog", "Card Preparation", "Production", "Tests QA", "Mise en live", "In Production", "Live (March 2016)" ]
+  );
+
+  const trelloActions$ = Observable.combineLatest(
+    trelloLists$,
     Trello.actions$.startWith( [] ),
     parseActions( moment().format( 'YYYY-MM-DD' ) )
   );
+  
+  const trelloData$ = Observable.combineLatest(
+    displayedLists$,
+    trelloActions$,
+    parseTrelloData
+  );
+
+  const lists$ = trelloLists$.map( R.map( R.propOr( "", "name" ) ) );
 
   return {
-    DOM: buttonView( buttonClicks$.startWith( false ) ),
+    DOM: Observable.combineLatest(
+      lists$,
+      firstDisplayedListChanges$,
+      lastDisplayedListChanges$,
+      ( lists, first, last ) =>
+        div( [
+          button( 'Get actions' ),
+          selectView( 'first-displayed-list', 'Work begins', first, lists ),
+          selectView( 'last-displayed-list', 'Work ends', last, lists )
+        ] )
+    ),
     Trello: buttonClicks$,
-    graph: trelloActions$.map( parseTrelloData( displayedLists ) ),
-    log: trelloActions$
+    graph: trelloData$,
+    log: lists$
   };
 }
 
