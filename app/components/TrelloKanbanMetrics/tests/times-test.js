d36cf18180c77c97d74473f75597999d5d65977b
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2016-04-08 13:53:26 +0200

    Add leadTimeFromDates()

--- app/components/TrelloKanbanMetrics/tests/times-test.js
+++ app/components/TrelloKanbanMetrics/tests/times-test.js
@@ -1,8 +1,44 @@
 import {test} from 'tape';
 
 import {
+  leadTimeFromDates,
   avgLeadTime
 } from '../times';
+
+test( 'leadTimeFromDates', ( assert ) => {
+  const dates = [
+    { list: "Backlog", date: "2016-04-01" },
+    { list: "Card Preparation [2]", date: "2016-04-01" },
+    { list: "Production [3]", date: "2016-04-02" },
+    { list: "Tests QA [2]", date: "2016-04-05" },
+    { list: "Mise en live [1]", date: "2016-04-05" },
+    { list: "In Production", date: "2016-04-06" },
+    { list: "Live (April 2016)", date: "2016-04-08" }
+  ];
+  const datesWithStartingNulls = [
+    { list: "Backlog", date: null },
+    { list: "Card Preparation [2]", date: null },
+    { list: "Production [3]", date: "2016-04-02" },
+    { list: "Tests QA [2]", date: "2016-04-05" },
+    { list: "Mise en live [1]", date: "2016-04-05" },
+    { list: "In Production", date: "2016-04-06" },
+    { list: "Live (April 2016)", date: "2016-04-08" }
+  ];
+  const datesWithEndingNulls = [
+    { list: "Backlog", date: "2016-04-01" },
+    { list: "Card Preparation [2]", date: "2016-04-01" },
+    { list: "Production [3]", date: "2016-04-02" },
+    { list: "Tests QA [2]", date: "2016-04-05" },
+    { list: "Mise en live [1]", date: "2016-04-05" },
+    { list: "In Production", date: null },
+    { list: "Live (April 2016)", date: null }
+  ];
+
+  assert.equals( leadTimeFromDates( dates ), 7, 'should return the lead time from dates' );
+  assert.equals( leadTimeFromDates( datesWithStartingNulls ), 6, 'should consider the first non-null date as the start date' );
+  assert.equals( leadTimeFromDates( datesWithEndingNulls ), null, 'should return null if last list has no date' );
+  assert.end();
+} );
 
 test( 'avgLeadTime', ( assert ) => {
   const dataAvgInteger = [
--- app/components/TrelloKanbanMetrics/times.js
+++ app/components/TrelloKanbanMetrics/times.js
@@ -1,5 +1,28 @@
 import R from 'ramda';
 
+import {daysSpent} from '../../utils/date';
+
+// _propDate :: {date: a} -> a
+const _propDate = R.prop( 'date' );
+
+// _isDateNil :: {date: a} -> Boolean
+const _isDateNil = R.compose( R.isNil, _propDate );
+
+// leadTimeFromDates :: [{list: String, date: Date}] -> Integer
+const leadTimeFromDates = R.cond( [
+  [ R.compose( _isDateNil, R.last ), R.always( null ) ],
+  [
+    R.T,
+    R.compose(
+      R.converge(
+        daysSpent,
+        [ R.compose( _propDate, R.head ), R.compose( _propDate, R.last ) ]
+      ),
+      R.reject( _isDateNil )
+    )
+  ]
+] );
+
 // avgLeadTime :: [{leadTime: Integer}] -> Integer
 const avgLeadTime = R.compose(
   Math.round.bind( Math ),
@@ -8,5 +31,6 @@ const avgLeadTime = R.compose(
 );
 
 export default {
+  leadTimeFromDates,
   avgLeadTime
 };
